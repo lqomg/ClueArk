65ba6db0fd65c2a4a934a16984f6beacc2c907a0
lqOmg <1432667757@qq.com>
2026-04-30 11:13:05 +0800

    fix: update GitHub repository URL and label to reflect correct project name

--- frontend/src/constants/site.ts
+++ frontend/src/constants/site.ts
@@ -1,3 +1,3 @@
 /** 官方开源仓库 */
-export const GITHUB_REPO_URL = 'https://github.com/lqomg/CuleArk';
-export const GITHUB_REPO_LABEL = 'github.com/lqomg/CuleArk';
+export const GITHUB_REPO_URL = 'https://github.com/lqomg/ClueArk';
+export const GITHUB_REPO_LABEL = 'github.com/lqomg/ClueArk';
